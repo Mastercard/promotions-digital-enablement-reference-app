016a25e3ed0780497d2c63d7ee2fb35a6640912a
e105602 <tapaswi.vulli@mastercard.com>
2023-09-11 12:50:32 -0500

    fixed build

--- src/main/java/com/mastercard/developer/service/TransactionService.java
+++ src/main/java/com/mastercard/developer/service/TransactionService.java
@@ -3,17 +3,17 @@ package com.mastercard.developer.service;
 import lombok.extern.slf4j.Slf4j;
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
-import org.openapitools.client.api.TransactionApi;
+import org.openapitools.client.api.TransactionsApi;
 import org.openapitools.client.model.PagedResponseGetTransactionDto;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class TransactionService {
-    private final TransactionApi transactionApi;
+    private final TransactionsApi transactionApi;
 
     public TransactionService(ApiClient apiClient) {
-        this.transactionApi = new TransactionApi(apiClient);
+        this.transactionApi = new TransactionsApi(apiClient);
     }
 
     public PagedResponseGetTransactionDto getTransactions(String accountId, String fromDate, String toDate, String promotionId, Integer offset, Integer limit) throws ApiException {
