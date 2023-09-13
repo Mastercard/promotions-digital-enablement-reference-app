b54e7ce072327bb6e9b05dbdafd068b772e40dcf
e105602 <tapaswi.vulli@mastercard.com>
2023-09-13 10:46:21 -0500

    removed wildcard imports

--- src/test/java/com/mastercard/developer/controller/TransactionControllerTest.java
+++ src/test/java/com/mastercard/developer/controller/TransactionControllerTest.java
@@ -1,33 +1,24 @@
 package com.mastercard.developer.controller;
 
 import com.mastercard.developer.exception.InvalidRequest;
-import com.mastercard.developer.service.ProgressService;
 import com.mastercard.developer.service.TransactionService;
-import com.mastercard.developer.validator.ProgressValidator;
 import com.mastercard.developer.validator.TransactionValidator;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openapitools.client.ApiException;
-import org.openapitools.client.model.*;
+import org.openapitools.client.model.PagedResponseGetTransactionDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -64,17 +55,17 @@ public class TransactionControllerTest {
     @Test
     public void testGetTransactions_Success() throws Exception {
         when(transactionService.getTransactions(anyString(), any(String.class), any(String.class),
-                anyString(), any(Integer.class),any(Integer.class) ))
+                anyString(), any(Integer.class), any(Integer.class)))
                 .thenReturn(new PagedResponseGetTransactionDto());
         PagedResponseGetTransactionDto response = controller.getTransactions(UUID.randomUUID().toString(), "2020-03-10",
                 "2020-05-20", UUID.randomUUID().toString(), 0, 1);
         assertNotNull(response);
     }
 
-    @Test (expected = InvalidRequest.class)
+    @Test(expected = InvalidRequest.class)
     public void testGetTransactions_Exception() throws Exception {
         when(transactionService.getTransactions(anyString(), any(String.class), any(String.class),
-                anyString(), any(Integer.class),any(Integer.class) ))
+                anyString(), any(Integer.class), any(Integer.class)))
                 .thenThrow(new ApiException());
         controller.getTransactions(UUID.randomUUID().toString(), "2020-03-10",
                 "2020-05-20", UUID.randomUUID().toString(), 0, 1);
