100b5ba6bd961f2b84d6f2a3437e46243bd8b0a2
e169407 <bhupendrakumar.verma@mastercard.com>
2025-01-24 15:37:06 +0530

    S5012793-Audience API Changes to Reference App in Github

---
+++ src/main/java/com/mastercard/developer/controller/AudienceController.java
@@ -0,0 +1,104 @@
+package com.mastercard.developer.controller;
+
+import com.mastercard.developer.exception.InvalidRequest;
+import com.mastercard.developer.service.AudienceService;
+import com.mastercard.developer.validator.AudienceValidator;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.model.Audience;
+import org.openapitools.client.model.AudienceUpdate;
+import org.openapitools.client.model.PagedResponseAudience;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Audiences")
+@Slf4j
+@RestController
+@NoArgsConstructor
+@AllArgsConstructor
+@RequestMapping(
+        value = "",
+        produces = {"application/json"}) //ToCheck
+public class AudienceController {
+
+    private AudienceValidator audienceValidator;
+    private AudienceService audienceService;
+
+    /*---------------------------------------------------------------- GET Audience API -------------------------------------------------------*/
+
+    @GetMapping(path = "/audiences")
+    public PagedResponseAudience getAudiences(
+            @RequestParam(name = "entity_id") String entityId,
+            @RequestParam(name = "entity_type") String entityType,
+            @RequestParam(name = "code", required = false) String audienceCode,
+            @RequestParam(name = "from_date_time", required = false) String fromDateTime,
+            @RequestParam(name = "to_date_time", required = false) String toDateTime,
+            @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
+            @RequestParam(value = "limit", required = false, defaultValue = "25") int limit) {
+        audienceValidator.validateAudienceGetDataRequest(fromDateTime, toDateTime, entityType, entityId);
+        try {
+            log.info("Method : getAudiences, Message : Getting Audiences");
+            PagedResponseAudience response = audienceService.getAudiencePagedExternalTargetRecords(audienceCode, entityId, entityType, fromDateTime, toDateTime, offset, limit);
+            if (response != null) {
+                log.debug("Method : getAudiences, Message :Successfully got the Audiences" + response);
+                return response;
+            }
+        } catch (ApiException ex) {
+            log.error("Method : getAudiences, Message : Failed to get the Audiences");
+            throw new InvalidRequest(ex.getResponseBody());
+        }
+        throw new InvalidRequest(HttpStatus.BAD_REQUEST);
+    }
+
+    /*---------------------------------------------------------------- POST Audience API -------------------------------------------------------*/
+
+    @PostMapping(
+            consumes = {"application/json"},
+            path = "/audiences")
+    public Audience createAudience(@RequestBody Audience audience) {
+        audienceValidator.validateAudienceCreate(audience);
+        try {
+            Audience response = audienceService.saveAudience(audience);
+            if (response != null && response.getId() != null) {
+                log.debug("Method : createAudience, Message :Successfully created the audience" + response);
+                return response;
+            }
+        } catch (ApiException ex) {
+            log.error("Method : createAudience, Message : Failed to created the audience");
+            throw new InvalidRequest(ex.getResponseBody());
+        }
+        throw new InvalidRequest(HttpStatus.BAD_REQUEST);
+    }
+
+    /*---------------------------------------------------------------- PUT Audience API -------------------------------------------------------*/
+
+    @PutMapping(
+            path = "/audiences/{id}",
+            consumes = {"application/json"})
+    public Audience updateAudience(
+            @PathVariable(value = "id") String referenceId,
+            @RequestBody AudienceUpdate audienceUpdate) {
+        audienceValidator.validateAudienceUpdateData(audienceUpdate);
+        try {
+            Audience response = audienceService.updateAudience(referenceId, audienceUpdate);
+            if (response != null && response.getId() != null) {
+                log.debug("Method : updateAudience, Message :Successfully updated the audience" + response);
+                return response;
+            }
+        } catch (ApiException ex) {
+            log.error("Method : updateAudience, Message : Failed to update the audience");
+            throw new InvalidRequest(ex.getResponseBody());
+        }
+        throw new InvalidRequest(HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/test/java/com/mastercard/developer/validator/AudienceValidatorTest.java
@@ -0,0 +1,143 @@
+package com.mastercard.developer.validator;
+
+import com.mastercard.developer.exception.InvalidRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openapitools.client.model.Audience;
+import org.openapitools.client.model.AudienceUpdate;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AudienceValidatorTest {
+
+    private static final LocalDateTime BEGIN_DATE = LocalDateTime.now();
+    private static final LocalDateTime END_DATE = LocalDateTime.now().plusDays(5);
+    private static final String BEGIN_DATE_TIME = BEGIN_DATE.atZone(ZoneOffset.UTC)
+            .withZoneSameInstant(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME);
+    private static final String END_DATE_TIME = END_DATE.atZone(ZoneOffset.UTC)
+            .withZoneSameInstant(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME);
+
+    @InjectMocks
+    private AudienceValidator validator;
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testValidateUpdateDataRequestForInvalidFormatOfBeginDate() {
+        AudienceUpdate audienceUpdate = createAudienceUpdateObj();
+        audienceUpdate.setBeginDateTime("2024-10-2108:08:08");
+        audienceUpdate.setEndDateTime(END_DATE_TIME);
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("Invalid Begin Date Time Format");
+        validator.validateAudienceUpdateData(audienceUpdate);
+    }
+
+    @Test
+    public void testValidatePostCreateRequestForInvalidFormatOfBeginDate() {
+        Audience audience = createAudienceObj();
+        audience.setBeginDateTime("2024-10-21T08:08:08Z");
+        audience.setEndDateTime("2024-10-12T08:08:08Z");
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("The End Date Time should be after the Begin Date Time");
+        validator.validateAudienceCreate(audience);
+    }
+
+    @Test
+    public void testValidateAudienceCreateForInvalidCode() {
+        Audience audience = createAudienceObj();
+        audience.setCode(null);
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Code cannot be null");
+        validator.validateAudienceCreate(audience);
+    }
+
+    @Test
+    public void testValidateAudienceCreateForInvalidBeginDate() {
+        Audience audience = createAudienceObj();
+        audience.setBeginDateTime(null);
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Begin Date cannot be null");
+        validator.validateAudienceCreate(audience);
+    }
+
+    @Test
+    public void testValidateAudienceCreateForNullEntityType() {
+        Audience audience = createAudienceObj();
+        audience.setEntityType(null);
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Entity Type cannot be null");
+        validator.validateAudienceCreate(audience);
+    }
+
+    @Test
+    public void testValidateAudienceCreateForInvalidEntityType() {
+        Audience audience = createAudienceObj();
+        audience.setEntityType("S");
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Entity Type must be either A or H");
+        validator.validateAudienceCreate(audience);
+    }
+
+    @Test
+    public void testValidateAudienceCreateForNullEntityId() {
+        Audience audience = createAudienceObj();
+        audience.setEntityId(null);
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Entity Reference Id cannot be null");
+        validator.validateAudienceCreate(audience);
+    }
+
+    private Audience createAudienceObj() {
+        Audience audience = new Audience();
+        audience.setBeginDateTime("2024-10-12T08:08:08Z");
+        audience.setCode("ABCODE");
+        audience.setEntityId("EntityId");
+        audience.setEntityType("A");
+        audience.setEndDateTime("2024-10-21T08:08:08Z");
+        return audience;
+    }
+
+    private AudienceUpdate createAudienceUpdateObj() {
+        AudienceUpdate audienceUpdate = new AudienceUpdate();
+        audienceUpdate.setBeginDateTime("2024-10-12T08:08:08Z");
+        audienceUpdate.setEndDateTime("2024-10-21T08:08:08Z");
+        return audienceUpdate;
+    }
+
+    @Test
+    public void testValidateGetRequestForNullEntityId() {
+        String fromDate = "2024-10-21T08:08:08Z";
+        String entityId = null;
+        String entityType = "H";
+        String toDate = "2024-10-12T08:08:08Z";
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Entity Reference Id cannot be null");
+        validator.validateAudienceGetDataRequest(fromDate, toDate, entityType, entityId);
+    }
+
+    @Test
+    public void testValidateGetRequestForNullEntityType() {
+        String fromDate = "2024-10-21T08:08:08Z";
+        String entityId = "AC30";
+        String entityType = null;
+        String toDate = "2024-10-12T08:08:08Z";
+        expectedEx.expect(InvalidRequest.class);
+        expectedEx.expectMessage("External Target Record Entity Type cannot be null");
+        validator.validateAudienceGetDataRequest(fromDate, toDate, entityType, entityId);
+    }
+}
