69c4c53bab5cd4689a5216976d10def4ad08feb1
e169407 <bhupendrakumar.verma@mastercard.com>
2025-01-28 02:30:45 +0530

    S5012793-Added service in constant package for test

---
+++ src/main/java/com/mastercard/developer/constants/AudienceService.java
@@ -0,0 +1,34 @@
+package com.mastercard.developer.constants;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.api.AudiencesApi;
+import org.openapitools.client.model.Audience;
+import org.openapitools.client.model.AudienceUpdate;
+import org.openapitools.client.model.PagedResponseAudience;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AudienceService {
+
+    private final AudiencesApi audiencesApi;
+
+    public AudienceService(ApiClient apiClient) {
+        this.audiencesApi = new AudiencesApi(apiClient);
+    }
+
+    public PagedResponseAudience getAudiencePagedExternalTargetRecords(String audienceCode, String entityId, String entityType,
+                                                                       String fromDateTime, String toDateTime, Integer offset, Integer limit) throws ApiException {
+        return audiencesApi.getAudiences(entityId, entityType, audienceCode, fromDateTime, toDateTime, offset, limit);
+    }
+
+    public Audience saveAudience(Audience audience) throws ApiException {
+        return audiencesApi.createAudience(audience);
+    }
+
+    public Audience updateAudience(String referenceId, AudienceUpdate audienceUpdate) throws ApiException {
+        return audiencesApi.updateAudience(referenceId, audienceUpdate);
+    }
+}
